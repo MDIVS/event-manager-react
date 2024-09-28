43b68b446666f221be7652f8c0afc198bebd0452
MDIVS <maiconoficialbr@gmail.com>
2024-09-28 18:10:10 -0300

    reworked input architecture

--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -1,26 +1,21 @@
 import { useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import axios from "axios";
-import InputUsername, {InputUsernameData} from "../fields/input-username";
-
-const REGEX_HAS_UPPER_AND_LOWER_CASE = /^(?=.*[a-z])(?=.*[A-Z])/;
-const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,24}/;
-const REGEX_HAS_NUMBER_AND_SPECIAL_CHAR = /^(?=.*\d)(?=.*[^a-zA-Z\d])/;
+import InputUsername from "../fields/input-username";
+import InputPassword from "../fields/input-password";
 
 export function Register() {
     const [userErr, setUserErr] = useState('');
-    let username:string = '';
-
-    const [pwd, setPwd] = useState('');
-    const [validPwd, setValidPwd] = useState(false);
+    const [username, setUsername] = useState('');
+    const [passErr, setPassErr] = useState('');
+    const [pass, setPass] = useState('');
 
     const [matchPwd, setMatchPwd] = useState('');
     const [validMatch, setValidMatch] = useState(false);
 
     useEffect(() => {
-        setValidPwd(REGEX_PASSWORD.test(pwd))
-        setValidMatch(pwd === matchPwd)
-    }, [pwd, matchPwd])
+        setValidMatch(pass === matchPwd)
+    }, [pass, matchPwd])
 
     function handleSubmit(event:React.FormEvent<HTMLFormElement>) {
         alert('Submit clicked!');
@@ -28,7 +23,7 @@ export function Register() {
         axios({
             method: 'POST',
             url: '/user',
-            data: {record: {username, password: pwd}}
+            data: {record: {username, password: pass}}
         }).then((res) => console.log(res.data));
         event.preventDefault();
     }
@@ -40,32 +35,12 @@ export function Register() {
 
             {/* <form onSubmit={handleSubmit}> */}
             <form onSubmit={handleSubmit}>
-                <InputUsername onDataChanged={(data:InputUsernameData) => {setUserErr(data.error); username = data.value;}}/>
+                <InputUsername onDataChanged={(data:InstanceType<typeof InputUsername.InputData>) => {setUserErr(data.error); setUsername(data.value);}}/>
                 <p className="text-red-400">{userErr}</p>
                 <br />
 
-                <label htmlFor="password"> create your password </label>
-                <input
-                    type="password"
-                    id="password"
-                    onChange={(e) => setPwd(e.target.value.replace(/\s/g, ""))}
-                    value={pwd}
-                    required
-                    aria-invalid={validPwd ? "false" : "true"}
-                    className={twMerge(
-                        "transition-all duration-500 w-full m-2 ms-0 me-0 p-1 bg-white bg-opacity-10 border-l-2 border-opacity-50 rounded-md outline-none focus:border-opacity-100",
-                        validPwd ? "border-green-500": pwd ? "border-red-600" : "border-yellow-600"
-                    )}
-                />
-                <div className="text-xs text-left transition-all duration-500">
-                    <p className={pwd ? pwd.length < 8 || pwd.length > 24 ? "text-orange-500" : "text-green-500" : ""}>
-                        • 8 to 24 characters</p>
-                    <p className={pwd ? !REGEX_HAS_UPPER_AND_LOWER_CASE.test(pwd) ? "text-orange-500" : "text-green-500" : ""}>
-                        • Must include uppercase and lowercase letters</p>
-                    <p className={pwd ? !REGEX_HAS_NUMBER_AND_SPECIAL_CHAR.test(pwd) ? "text-orange-500" : "text-green-500" : ""}>
-                        • Must include a number and a special character</p>
-                </div>
-
+                <InputPassword onDataChanged={(data:InstanceType<typeof InputPassword.InputData>) => {setPassErr(data.error); setPass(data.value);}}/>
+                <p className="text-red-400">{passErr}</p>
                 <br />
 
                 <label htmlFor="confirm_pwd"> confirm your password </label>
@@ -83,7 +58,7 @@ export function Register() {
                     )}
                 />
                 <div className="text-xs">
-                    <p className={matchPwd ? matchPwd != pwd ? "text-orange-500" : "text-green-500" : ""}>
+                    <p className={matchPwd ? matchPwd != pass ? "text-orange-500" : "text-green-500" : ""}>
                         Passwords must match
                     </p>
                 </div>
@@ -91,8 +66,8 @@ export function Register() {
                 <br />
                 
                 <button className="transition-all duration-300 outline-none border-2 border-yellow-400 border-opacity-50 text-yellow-400 rounded-md p-1 px-16 bg-white bg-opacity-5 hover:border-opacity-100 focus:border-opacity-100 disabled:border-white disabled:border-opacity-50 disabled:opacity-35"
-                    disabled={userErr || !validPwd || !validMatch ? true : false}>
-                        Sign Up
+                    disabled={(username==='' || userErr || pass==='' || passErr || !validMatch) ? true : false}>
+                    Sign Up
                 </button>
             </form>
 
--- src/components/fields/input-password.tsx
+++ src/components/fields/input-password.tsx
@@ -1,17 +1,10 @@
 import React, { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-const USER_REGEX = /^[A-z][A-z0-9-_]{2,23}$/;
 const MATCH_ALL_WHITESPACE_CHARACTERS = /\s/g;
-const REGEX_STARTS_WITH_LETTER = /^[A-Za-z]/;
-const REGEX_CONTAINS_SPECIAL_CHARACTERS = /[^\w-]/; // check if a string contains special characters except for "-" and "_"
-
-function hasAccentedChar(inputString:string) {
-    const accentedCharacters = "ÁáÀàÉéÈèÍíÌìÓóÒòÚúÙùÇçÑñ";
-    for (const char of inputString) if (accentedCharacters.includes(char))
-        return true
-    return false;
-}
+const REGEX_HAS_UPPER_AND_LOWER_CASE = /^(?=.*[a-z])(?=.*[A-Z])/;
+const REGEX_PASSWORD = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,24}/;
+const REGEX_HAS_NUMBER_AND_SPECIAL_CHAR = /^(?=.*\d)(?=.*[^a-zA-Z\d])/;
 
 interface InputData {
     value: string;
@@ -23,7 +16,7 @@ interface InputProps extends ComponentProps<'input'> {
     onDataChanged: (data:InputData) => void;
 }
 
-export default class InputUsername extends React.Component<InputProps> {
+export default class InputPassword extends React.Component<InputProps> {
     constructor(props: InputProps) {
         super(props);
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -52,10 +45,9 @@ export default class InputUsername extends React.Component<InputProps> {
         if (prevState.value !== value || prevState.isValid !== isValid) { // is value changed?
             if (value === '' || isValid) this.props.onDataChanged({ value, isValid, error: '' });
             else {
-                if (value.length < 3 || value.length > 24) this.props.onDataChanged({ value, isValid, error: '3 to 24 characters' });
-                else if (!REGEX_STARTS_WITH_LETTER.test(value)) this.props.onDataChanged({value, isValid, error:'Must begin with a letter'});
-                else if (hasAccentedChar(value)) this.props.onDataChanged({value, isValid, error:'Accented letters are not allowed'});
-                else if (REGEX_CONTAINS_SPECIAL_CHARACTERS.test(value)) this.props.onDataChanged({value, isValid, error:'Only underscores and hyphens symbols allowed'});
+                if (value.length < 8 || value.length > 24) this.props.onDataChanged({ value, isValid, error: '8 to 24 characters' });
+                else if (!REGEX_HAS_UPPER_AND_LOWER_CASE.test(value)) this.props.onDataChanged({ value, isValid, error: 'Must include uppercase and lowercase letters' });
+                else if (!REGEX_HAS_NUMBER_AND_SPECIAL_CHAR.test(value)) this.props.onDataChanged({ value, isValid, error: 'Must include a number and a special character' });
             }
         }
     }
@@ -64,22 +56,22 @@ export default class InputUsername extends React.Component<InputProps> {
         const newValue = e.target.value.replace(MATCH_ALL_WHITESPACE_CHARACTERS, "");
         this.setState({
             value: newValue,
-            isValid: USER_REGEX.test(newValue)
+            isValid: REGEX_PASSWORD.test(newValue)
         });
     }
 
     render() {
         return (
             <input
-                type="text"
-                id="username"
-                placeholder="Username"
+                type="password"
+                id="password"
+                placeholder="Password"
                 onChange={this.handleInputChange}
                 value={this.state.value}
                 aria-invalid={this.state.isValid ? "false" : "true"}
                 className={twMerge(
-                    "transition-all duration-500 border-yellow-600 w-full m-2 ms-0 me-0 p-1 bg-white bg-opacity-10 border-l-2 border-opacity-50 rounded-md outline-none focus:border-opacity-100",
-                    this.state.isValid ? "border-green-500": this.state.value ? "border-red-600" : ""
+                    "transition-all duration-500 w-full m-2 ms-0 me-0 p-1 bg-white bg-opacity-10 border-l-2 border-opacity-50 rounded-md outline-none focus:border-opacity-100",
+                    this.state.isValid ? "border-green-500": this.state.value ? "border-red-600" : "border-yellow-600"
                 )}
                 {...this.props}
             />
